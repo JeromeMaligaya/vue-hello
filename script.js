682f782e06caff897a96e95c47e8a5fc55763a8a
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-03 16:35:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1.recupero l'elemento dal Dom
+
+//2.raccolta dati
+    
+// 3.lavorazione dati 
+
+// 4.generazione output
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
